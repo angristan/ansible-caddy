434c25c831d9e902543e986ebb86e9affe660508
Stanislas Lange <stanislas.lange@pm.me>
2020-10-16 21:39:08 +0200

    Update build script to Caddy 2.2.1

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker run --rm -v `pwd`/bin:/output caddy:2.2.0-builder sh -c "caddy-builder github.com/caddy-dns/cloudflare && cp /usr/bin/caddy /output/"
+docker run --rm -v `pwd`/bin:/output caddy:2.2.1-builder sh -c "caddy-builder github.com/caddy-dns/cloudflare && cp /usr/bin/caddy /output/"
