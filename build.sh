e4d18d2e6aba6d87de9aa8179bcdddbaf25bbfc4
Stanislas Lange <stanislas.lange@pm.me>
2020-11-29 22:02:44 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-docker run --rm -v `pwd`/bin:/output caddy:2.2.1-builder sh -c "caddy-builder github.com/caddy-dns/cloudflare && cp /usr/bin/caddy /output/"
+# docker run --rm -v $(pwd)/bin:/output caddy:2.2.1-builder sh -c "caddy-builder github.com/caddy-dns/cloudflare github.com/greenpau/caddy-auth-jwt github.com/greenpau/caddy-auth-portal && cp /usr/bin/caddy /output/"
+
+export GOOS=linux
+export GOARCH=amd64
+
+xcaddy build --with github.com/caddy-dns/cloudflare --with github.com/greenpau/caddy-auth-jwt --with github.com/greenpau/caddy-auth-portal --output bin/caddy
